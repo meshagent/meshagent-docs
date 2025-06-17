8630f8f5a4bd3636020ff935f695f88cbc1d49dc
Jesse Ezell <jesse.ezell@timu.com>
2025-06-16 20:15:27 -0700

    fix local shell tool

---
+++ examples/python/services/codex_chatbot/main.py
@@ -0,0 +1,70 @@
+from meshagent.api import RequiredToolkit, RequiredSchema
+from meshagent.tools import Toolkit
+from meshagent.agents.schemas.gallery import gallery_schema
+from meshagent.tools.storage import SaveFileFromUrlTool
+from meshagent.tools.document_tools import DocumentAuthoringToolkit, DocumentTypeAuthoringToolkit
+from meshagent.agents.chat import ChatBot
+from meshagent.openai import OpenAIResponsesAdapter
+from meshagent.openai.tools.responses_adapter import LocalShellTool
+
+from meshagent.agents.thread_schema import thread_schema
+
+import asyncio
+import os
+
+from meshagent.api import SchemaRegistry, SchemaRegistration
+
+from meshagent.api.services import ServiceHost
+
+service = ServiceHost()
+
+@service.path("/thread")
+class ThreadSchema(SchemaRegistry):
+    def __init__(self):
+        super().__init__(
+            name="thread",
+            schemas=[
+                SchemaRegistration(
+                    name="thread",
+                    schema=thread_schema
+                )
+            ]
+        )
+
+@service.path("/agent")
+class CodexChatbot(ChatBot):
+    def __init__(self):
+        super().__init__(
+            
+            name="meshagent.codex-chatbot",
+            title="image designer",
+            description="an agent that generates images and videos",
+            empty_state_title="What images can I make for you?",
+            rules=[
+                "you are an assistant for generating images"
+            ],
+            llm_adapter = OpenAIResponsesAdapter(parallel_tool_calls=True, model="codex-mini-latest"),
+            requires = [
+            ],
+            toolkits=[
+                
+                Toolkit(name="local", tools=[
+                    SaveFileFromUrlTool()
+                ]),
+            ],
+            auto_greet_message="What images can I help you design?",
+            labels=[ "tasks", "images" ]
+        )
+
+    async def get_thread_toolkits(self, *, thread_context, participant):
+        return [
+            Toolkit(
+                name="builtin",
+                tools=[
+                    LocalShellTool()
+                ]
+            )
+        ]
+    
+
+asyncio.run(service.run())
